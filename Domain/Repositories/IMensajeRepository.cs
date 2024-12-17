f6f5b6981790547fd601f7ac4b73276077d93cad
Octavio Ulla <oulla@ulasoft.com.ar>
2024-12-17 20:19:03 -0300

    feat(PI-5): Interfaz repositorio mensajes creada

---
+++ Domain/Repositories/IMensajeRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Repositories
+{
+    internal interface IMensajeRepository
+    {
+        /// <summary>
+        /// Guarda un mensaje en la DB
+        /// </summary>
+        /// <param name="mensaje">Mensaje a guardar en la DB</param>
+        Task SaveAsync(Mensaje mensaje);
+    }
+}
