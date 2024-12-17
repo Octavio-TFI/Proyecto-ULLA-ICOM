8f1979302a32f36714d90ea8969e55a097483005
Octavio Ulla <oulla@ulasoft.com.ar>
2024-12-17 20:31:19 -0300

    feat(PI-5): IUnitOfWork interfaz creada

---
+++ Domain/Repositories/IUnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Repositories
+{
+    internal interface IUnitOfWork
+    {
+        /// <summary>
+        /// Repositorio de mensajes
+        /// </summary>
+        IMensajeRepository Mensajes { get; }
+
+        /// <summary>
+        /// Guarda los cambios en la DB
+        /// </summary>
+        Task SaveChangesAsync();
+    }
+}
