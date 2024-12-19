9e404f594d2ff67743f392ba3a41f8e3dc0bea30
Octavio Ulla <oulla@ulasoft.com.ar>
2024-12-18 21:01:58 -0300

    feat(PI-5): Registracion capa de controllers

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,16 @@
+using Controllers;
+using System.Reflection;
+using System.Reflection.Metadata;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+var controllersAssembly = Assembly.GetAssembly(typeof(TestController))
+    ?? throw new Exception("Assembly de Controllers no econtrada");
+
+builder.Services.AddControllers().AddApplicationPart(controllersAssembly);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
