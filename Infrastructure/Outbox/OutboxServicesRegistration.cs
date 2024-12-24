bea4e2e052d81950683d02b2bcd3da8ede6bad54
Octavio Ulla <oulla@ulasoft.com.ar>
2024-12-23 21:05:36 -0300

    feat(PI-9): Registro outbox service

---
+++ Infrastructure/Outbox/OutboxServicesRegistration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Outbox
+{
+    public static class OutboxServicesRegistration
+    {
+        public static IServiceCollection AddOutboxServices(
+            this IServiceCollection services)
+        {
+            services.AddHostedService<OutboxService>();
+
+            return services;
+        }
+    }
+}
