88a4c1c8b4f270beac746ea5a9add04153e6314d
Octavio Ulla <oulla@ulasoft.com.ar>
2024-12-28 12:53:49 -0300

    feat(PI-9): Mensaje Recibido Handler implementado

--- AppServices/EventHandlers/MensajeRecibidoHandler.cs
+++ AppServices/EventHandlers/MensajeRecibidoHandler.cs
@@ -1,5 +1,9 @@
-﻿using Domain.Events;
+﻿using AppServices.Ports;
+using Domain.Events;
+using Domain.Repositories;
 using MediatR;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,13 +12,24 @@ using System.Threading.Tasks;
 
 namespace AppServices.EventHandlers
 {
-    internal class MensajeRecibidoHandler : INotificationHandler<MensajeRecibidoEvent>
+    internal class MensajeRecibidoHandler(
+        IMensajeRepository _mensajeRepository,
+        IGeneradorRespuesta _generadorRespuesta) : INotificationHandler<MensajeRecibidoEvent>
     {
-        public Task Handle(
+        public async Task Handle(
             MensajeRecibidoEvent request,
             CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            // Obtener ultimos mensajes del chat
+            var mensajes = await _mensajeRepository.GetUltimosMensajesChatAsync(
+                request.ChatId);
+
+            // Generar respuesta
+            var respuesta = await _generadorRespuesta.GenerarRespuestaAsync(
+                mensajes);
+
+            // Almacenar respuesta
+            await _mensajeRepository.InsertAsync(respuesta);
         }
     }
 }
