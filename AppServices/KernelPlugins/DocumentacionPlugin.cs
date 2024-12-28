839fc16b6026226a1ebcd9e680dd2621acb7aa57
Octavio Ulla <oulla@ulasoft.com.ar>
2024-12-28 19:31:39 -0300

    feat(PI-9): Documentacion puglin creado

---
+++ AppServices/KernelPlugins/DocumentacionPlugin.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using Microsoft.SemanticKernel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppServices.KernelPlugins
+{
+    internal class DocumentacionPlugin
+    {
+        [KernelFunction("Buscar Documentacion")]
+        [Description("Busca documentación relevante a la consulta")]
+        public async Task<IEnumerable<EmbeddingDoc>> BuscarDocumentacionAsync(
+            string consulta)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Domain/Entities/EmbeddingDoc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class EmbeddingDoc : Entity
+    {
+        public required string Titulo { get; set; }
+
+        public required string Texto { get; set; }
+
+        public required string EmbeddingTitulo { get; set; }
+
+        public required string EmbeddingTexto { get; set; }
+    }
+}
