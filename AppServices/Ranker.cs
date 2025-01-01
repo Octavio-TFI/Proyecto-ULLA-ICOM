5df6d389d0037e66f2b10f3d5eeaea1b295d6307
Octavio Ulla <oulla@ulasoft.com.ar>
2025-01-01 20:05:08 -0300

    feat(PI-10): Ranker creado y registrado

---
+++ AppServices/Ranker.cs
@@ -0,0 +1,22 @@
+﻿using AppServices.Abstractions;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppServices
+{
+    internal class Ranker
+        : IRanker
+    {
+        public Task<List<T>> RankAsync<T>(
+            List<T> datosRecuperados,
+            string consulta)
+            where T : Entity
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
