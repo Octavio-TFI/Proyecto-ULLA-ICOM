f146f3c6149fa5637319b1c96500e5c671d907e0
Octavio Ulla <oulla@ulasoft.com.ar>
2025-01-18 19:01:08 -0300

    feat(PI-20): Proyecto ChatApp(Interfaz de prueba) creada

---
+++ ChatApp/DTOs/MessageRecieved.cs
@@ -0,0 +1,9 @@
+﻿namespace ChatApp.DTOs
+{
+    public class MessageRecieved
+    {
+        public required Guid ChatId { get; init; }
+
+        public required string Text { get; init; }
+    }
+}
