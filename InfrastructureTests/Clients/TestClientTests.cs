03bdbe7df754bb2ad8e8e8d6a784537ea899d46f
Octavio Ulla <oulla@ulasoft.com.ar>
2025-01-20 22:59:57 -0300

    test(PI-13): TestClient test creados

---
+++ InfrastructureTests/Clients/TestClientTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Clients.Tests
+{
+    internal class TestClientTests
+    {
+        [Test]
+        public async Task EnviarMensajeTextoAsyncTest()
+        {
+            // Arrange
+            var mensaje = new MensajeTexto
+            {
+                ChatId = 1,
+                Texto = "Texto",
+                DateTime = DateTime.Now,
+                Tipo = TipoMensaje.Asistente
+            };
+
+            var httpClientFactoryMock = new Mock<IHttpClientFactory>();
+
+            var testClient = new TestClient(httpClientFactoryMock.Object);
+
+            // Act
+            await testClient.EnviarMensajeAsync(
+                "chatPlataformaId",
+                "usuarioId",
+                mensaje);
+
+            // Assert
+            Assert.Fail();
+        }
+    }
+}
