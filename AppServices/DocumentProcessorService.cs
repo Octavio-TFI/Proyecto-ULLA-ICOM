25ad37592a045020aaa2828cebc93e48fa834710
Octavio Ulla <oulla@ulasoft.com.ar>
2025-01-29 20:39:47 -0300

    feat(PI-8): DirectoryManager creado

--- AppServices/DocumentProcessorService.cs
+++ AppServices/DocumentProcessorService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using AppServices.Ports;
+using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace AppServices
 {
-    internal class DocumentProcessorService
+    internal class DocumentProcessorService(IFileManager _fileManager)
         : BackgroundService
     {
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
--- Infrastructure/FileSystem/FileManager.cs
+++ Infrastructure/FileSystem/FileManager.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Infrastructure.FileManager
+namespace Infrastructure.FileSystem
 {
     internal class FileManager
         : IFileManager
