017976b850b453072dd5c6075fa533a3b04fa26f
Octavio Ulla <oulla@ulasoft.com.ar>
2025-02-12 10:17:17 -0300

    test(PI-8): Añadir pruebas para HtmlProcessor
    Se introduce una nueva clase de prueba `HtmlProcessorTests` en el espacio de nombres `AppServices.DocumentProcessing.Tests` para probar la funcionalidad de `HtmlProcessor`. Se añaden directivas `using` necesarias. Se define una constante `HTML` con un documento HTML de muestra. Se agrega el método de prueba `ProcessAsync_WhenCalled_ReturnsDocumentList` que verifica la función `ProcessAsync` de `HtmlProcessor`, configurando un entorno de prueba con un `IDocumentProcessor` simulado y validando los resultados esperados.

---
+++ AppServicesTests/DocumentProcessing/HtmlProcessorTests.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ude;
+
+namespace AppServices.DocumentProcessing.Tests
+{
+    internal class HtmlProcessorTests
+    {
+        const string HTML = @"<html>
+<body>
+    <div id=""encabezadoPagina"">
+	<table cellspacing=""5"" class=""tablaEncabezado"">
+	    <tbody><tr>
+		    <td class=""c1E""></td>
+		    <td class=""c2E""><div id=""tituloPagina"">Test table title</div></td>
+        </tr></tbody>
+    </table>
+    </div>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <p>§ Test item list</p>
+
+    <h2>Test h2</h1>
+
+    <blockquote>
+        <p>Test blockquote</p>
+    </blockquote>
+
+    <a href=""../a.htm"" >URL Test</a>
+    <a href=""/a.htm"" >URL Test 2</a>
+
+    <p>|  | CAPATAZ en YouTube | |</p>
+    <p>|---|---|---|</p>
+</body>
+</html>";
+
+        [Test]
+        public async Task ProcessAsync_WhenCalled_ReturnsDocumentList()
+        {
+            // Arrange
+            var path = "path";
+
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            var documentData = Encoding.Latin1.GetBytes(HTML);
+            var charsetDetectorFactory = new Func<ICharsetDetector>(
+                () => new CharsetDetector());
+            var markdownProcessor = new Mock<IDocumentProcessor>();
+
+            markdownProcessor.Setup(
+                x => x.ProcessAsync(It.IsAny<string>(), It.IsAny<byte[]>()))
+                .ReturnsAsync(
+                    (string mdPath, byte[] data) =>
+                    {
+                        return [new Document()
+                        {
+                            Filename = mdPath,
+                            Texto = Encoding.UTF8.GetString(data),
+                            Embedding = [1,2,3]
+                        }];
+                    });
+
+            var htmlProcessor = new HtmlProcessor(
+                charsetDetectorFactory,
+                markdownProcessor.Object);
+
+            // Act
+            var result = await htmlProcessor.ProcessAsync(path, documentData);
+
+            // Assert
+            var firstDocument = result.First();
+
+            Assert.That(firstDocument.Filename, Is.EqualTo(path));
+            Assert.That(
+                firstDocument.Texto,
+                Is.EqualTo(
+                    @"# Test table title
+
+- Test item list
+
+## Test h2
+
+
+
+Test blockquote
+
+
+
+    URL Test
+    URL Test 2
+"));
+            Assert.That(
+                firstDocument.Embedding,
+                Is.EqualTo(new float[] { 1, 2, 3 }));
+        }
+    }
+}
