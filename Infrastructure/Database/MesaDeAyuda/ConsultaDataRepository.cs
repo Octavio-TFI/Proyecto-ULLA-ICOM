1986ffafcd20c6bcdaaf2b2dfd15018d4024e97a
Octavio Ulla <oulla@ulasoft.com.ar>
2025-02-18 20:57:37 -0300

    feat(PI-8): Se creo ConsultaDataRepository

---
+++ Infrastructure/Database/MesaDeAyuda/ConsultaDataRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Database.MesaDeAyuda
+{
+    internal class ConsultaDataRepository
+        : IConsultaDataRepository
+    {
+        public Task<List<ConsultaData>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ InfrastructureTests/Database/MesaDeAyuda/ConsultaDataRepositoryTests.cs
@@ -0,0 +1,31 @@
+﻿using AppServices.Ports;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Database.MesaDeAyuda.Tests
+{
+    internal class ConsultaDataRepositoryTests
+    {
+        [Test]
+        public async Task GetAllAsync()
+        {
+            // Arrange
+            var fileManagerMock = new Mock<IFileManager>();
+
+            fileManagerMock.Setup(x => x.ReadAllTextAsync("MesaDeAyuda.xml"))
+                .ReturnsAsync(string.Empty);
+
+
+            var consultaDataRepository = new ConsultaDataRepository();
+
+            // Act
+            var consultaDatas = await consultaDataRepository.GetAllAsync();
+
+            // Assert
+            Assert.That(consultaDataRepository, Is.Empty);
+        }
+    }
+}
