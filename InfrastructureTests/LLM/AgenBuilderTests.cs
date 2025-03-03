912068f870c1fc8e8937694b4e05c61153c77910
Octavio Ulla <oulla@ulasoft.com.ar>
2025-03-02 22:45:31 -0300

    test(PI-11): Clase de tests de AgentBuilder creado

---
+++ InfrastructureTests/LLM/AgenBuilderTests.cs
@@ -0,0 +1,34 @@
+﻿using AppServices.Ports;
+using Infrastructure.LLM.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.LLM.Tests
+{
+    class AgenBuilderTests
+    {
+        [Test]
+        public void BuildTest()
+        {
+            // Arrange
+            var serviceProvider = new Mock<IServiceProvider>();
+            var fileManager = new Mock<IFileManager>();
+            var chatHistoryFactory = new Mock<IChatHistoryAdapter>();
+
+            var agentBuilder = new AgentBuilder(
+                serviceProvider.Object,
+                fileManager.Object,
+                chatHistoryFactory.Object);
+
+            // Act
+            var agent = agentBuilder
+                .Build("name", TipoLLM.Pequeño);
+
+            // Assert
+            Assert.Fail();
+        }
+    }
+}
