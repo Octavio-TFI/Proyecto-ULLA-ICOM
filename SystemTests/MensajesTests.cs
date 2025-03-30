02ca9c0ecfc6adb427474e63c700437dce4f0dac
Octavio Ulla <oulla@ulasoft.com.ar>
2025-03-30 20:31:20 -0300

    test(PI-11): Test de sistema recuperacion de documentos y consultas

--- SystemTests/MensajesTests.cs
+++ SystemTests/MensajesTests.cs
@@ -1,5 +1,7 @@
 using Controllers.DTOs;
 using Domain.Entities.ChatAgregado;
+using Domain.Entities.ConsultaAgregado;
+using Domain.Entities.DocumentoAgregado;
 using FluentAssertions;
 using Infrastructure.Database;
 using Infrastructure.LLM.DTOs;
@@ -154,6 +156,49 @@ namespace SystemTests
             using var localLLMServer = WireMockServer.Start();
             using var chatServer = WireMockServer.Start();
 
+            var apiFactory = CreateAPIFactory(
+                localLLMServer.Port,
+                chatServer.Port);
+
+            // Agregar documentacion y consulta
+            var context = apiFactory.Services.CreateScope().ServiceProvider
+                .GetRequiredService<ChatContext>();
+
+            Guid documentoId = Guid.NewGuid();
+            Guid consultaId = Guid.NewGuid();
+
+            context.Documents
+                .Add(
+                    new Document
+                    {
+                        Id = documentoId,
+                        Texto = "Documento",
+                        Filename = "doc1.txt",
+                        Chunks =
+                            [new()
+                                {
+                                    Id = Guid.NewGuid(),
+                                    Texto = "Texto del documento",
+                                    Embedding = [1,2,3]
+                                }]
+                    });
+
+            context.Consultas
+                .Add(
+                    new Consulta
+                    {
+                        Id = consultaId,
+                        RemoteId = 1,
+                        EmbeddingTitulo = [1, 2, 3],
+                        EmbeddingDescripcion = [1, 2, 3],
+                        Titulo = "Titulo de la consulta",
+                        Descripcion = "Descripcion de la consulta",
+                        Solucion = "Solucion"
+                    });
+
+            context.SaveChanges();
+            context.Dispose();
+
             localLLMServer
                 .Given(Request.Create().WithPath("/v1/embeddings").UsingPost())
                 .RespondWith(
@@ -172,16 +217,15 @@ namespace SystemTests
                                 }));
 
             // Mock llamada de herramienta de información
-            localLLMServer
-                .Given(
-                    Request.Create()
-                        .WithPath("/v1/chat/completions")
-                        .WithBody(
-                            new JsonPathMatcher(
-                                    "$.messages[?(@.content == 'Chat')]",
-                                    "$.messages[?(@.content == 'Que es una orden de trabajo')]",
-                                    "$.tools"))
-                        .UsingPost())
+            localLLMServer.Given(
+                Request.Create()
+                    .WithPath("/v1/chat/completions")
+                    .WithBody(
+                        new JsonPathMatcher(
+                                "$.messages[?(@.content == 'Chat')]",
+                                "$.messages[?(@.content == 'Que es una orden de trabajo')]",
+                                "$.tools"))
+                    .UsingPost())
                 .RespondWith(
                     Response.Create()
                         .WithSuccess()
@@ -213,10 +257,71 @@ namespace SystemTests
     }
   ]
 }"));
+            // Mock ranker
+            localLLMServer.Given(
+                Request.Create()
+                    .WithPath("/v1/chat/completions")
+                    .WithBody(
+                        new JsonPathMatcher(
+                                "$.messages[?(@.role == 'system' && @.content == 'Documento')]"))
+                    .UsingPost())
+                .RespondWith(
+                    Response.Create()
+                        .WithSuccess()
+                        .WithBody(
+                            @"{
+              ""id"": ""chatcmpl-B9MHDbslfkBeAs8l4bebGdFOJ6PeG"",
+              ""object"": ""chat.completion"",
+              ""created"": 1741570283,
+              ""model"": ""gpt-4o-2024-08-06"",
+              ""choices"": [
+                {
+                  ""index"": 0,
+                  ""message"": {
+                    ""role"": ""assistant"",
+                    ""content"": ""{\""Score\"": true}"",
+                    ""refusal"": null,
+                    ""annotations"": []
+                  },
+                  ""logprobs"": null,
+                  ""finish_reason"": ""stop""
+                }
+              ]
+            }"));
+
+            localLLMServer.Given(
+                Request.Create()
+                    .WithPath("/v1/chat/completions")
+                    .WithBody(
+                        new JsonPathMatcher(
+                                "$.messages[?(@.role == 'system' && @.content != 'Documento' && @.content != 'Chat')]"))
+                    .UsingPost())
+                .RespondWith(
+                    Response.Create()
+                        .WithSuccess()
+                        .WithBody(
+                            @"{
+              ""id"": ""chatcmpl-B9MHDbslfkBeAs8l4bebGdFOJ6PeG"",
+              ""object"": ""chat.completion"",
+              ""created"": 1741570283,
+              ""model"": ""gpt-4o-2024-08-06"",
+              ""choices"": [
+                {
+                  ""index"": 0,
+                  ""message"": {
+                    ""role"": ""assistant"",
+                    ""content"": ""{\""Score\"": false}"",
+                    ""refusal"": null,
+                    ""annotations"": []
+                  },
+                  ""logprobs"": null,
+                  ""finish_reason"": ""stop""
+                }
+              ]
+            }"));
 
             // Mock respuesta con datos de la herramienta
-            localLLMServer
-            .Given(
+            localLLMServer.Given(
                 Request.Create()
                     .WithPath("/v1/chat/completions")
                     .WithBody(
@@ -249,10 +354,6 @@ namespace SystemTests
             chatServer.Given(Request.Create().WithPath("/Chat").UsingPost())
                 .RespondWith(Response.Create().WithSuccess());
 
-            var apiFactory = CreateAPIFactory(
-                localLLMServer.Port,
-                chatServer.Port);
-
             var client = apiFactory.CreateClient();
 
             var mensajeDTO = new MensajeTextoPrueba
@@ -266,21 +367,29 @@ namespace SystemTests
             var httpResponse = await client.PostAsJsonAsync("/Test", mensajeDTO)
                 .ConfigureAwait(false);
 
-            var dbContext = apiFactory.Services.CreateScope().ServiceProvider
+            context = apiFactory.Services.CreateScope().ServiceProvider
                 .GetRequiredService<ChatContext>();
 
-            while (dbContext.Set<Mensaje>().Count() < 2)
+            while (context.Set<Mensaje>().Count() < 2)
             {
                 await Task.Delay(100).ConfigureAwait(false);
             }
 
             // Assert
-            var chat = dbContext.Chats.Include(c => c.Mensajes).FirstOrDefault();
+            var chat = context.Chats.Include(c => c.Mensajes).FirstOrDefault();
             var mensajeUsuario = chat?.Mensajes.OrderBy(m => m.DateTime)
                 .FirstOrDefault();
             var mensajeIA = chat?.Mensajes.OrderBy(m => m.DateTime)
                 .Skip(1)
                 .FirstOrDefault();
+
+            context.Entry((MensajeIA)mensajeIA!)
+                .Collection(m => m.DocumentosRecuperados)
+                .Load();
+
+            context.Entry((MensajeIA)mensajeIA!)
+                .Collection(m => m.ConsultasRecuperadas)
+                .Load();
 
             Assert.Multiple(
                 () =>
@@ -297,7 +406,17 @@ namespace SystemTests
                         mensajeIA,
                         Is.TypeOf<MensajeIA>().And
                                 .Matches<MensajeIA>(
-                                    m => m.Texto == "Hola soy el test"));
+                                    m => m.Texto == "Hola soy el test" &&
+                                                m.DocumentosRecuperados
+                                                    .Any(
+                                                        d => d.DocumentoId ==
+                                                                            documentoId &&
+                                                                            d.Rank) &&
+                                                m.ConsultasRecuperadas
+                                                    .Any(
+                                                        c => c.ConsultaId ==
+                                                                            consultaId &&
+                                                                            !c.Rank)));
                 });
 
             while (chatServer.FindLogEntries(
