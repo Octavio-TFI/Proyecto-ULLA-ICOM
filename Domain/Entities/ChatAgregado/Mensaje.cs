67287ded22f24dd23e5778b353efa9681698da66
Octavio Ulla <oulla@ulasoft.com.ar>
2025-04-19 17:13:41 -0300

    feat(PI-25): PlataformaMensajeId agregado a entidad mensaje

--- Domain/Entities/ChatAgregado/Mensaje.cs
+++ Domain/Entities/ChatAgregado/Mensaje.cs
@@ -8,8 +8,14 @@ using System.Threading.Tasks;
 
 namespace Domain.Entities.ChatAgregado
 {
-    public abstract class Mensaje : Entity
+    public abstract class Mensaje
+        : Entity
     {
+        /// <summary>
+        /// Id del mensaje en la plataforma
+        /// </summary>
+        public string? PlataformaMensajeId { get; set; }
+
         /// <summary>
         /// DateTime de cuando se recibio el mensaje
         /// </summary>
---
+++ Infrastructure/Database/Repositories/MensajeIARepository.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities.ChatAgregado;
+using Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Database.Repositories
+{
+    internal class MensajeIARepository
+        : IMensajeIARepository
+    {
+        public Task<MensajeIA> GetAsync(
+            string mensajePlataformaId,
+            string plataforma)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
