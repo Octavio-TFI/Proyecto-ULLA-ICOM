b8b260943092e40069ddb811782ccea3246b1837
Octavio Ulla <oulla@ulasoft.com.ar>
2025-04-19 16:18:58 -0300

    feat(PI-25): Abstracciones en capa de app services para el caso de uso de calificar mensajes

---
+++ AppServices.Abstractions/DTOs/CalificacionMensajeDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppServices.Abstractions.DTOs
+{
+    public record CalificacionMensajeDTO
+    {
+        /// <summary>
+        /// Platforma utilizada para enviar el mensaje
+        /// </summary>
+        public required string Plataforma { get; init; }
+
+        /// <summary>
+        /// Identificador del mensaje en la plataforma
+        /// </summary>
+        public required string IdMensajePlataforma { get; init; }
+
+        /// <summary>
+        /// Calificación del mensaje
+        /// </summary>
+        public required bool Calificacion { get; init; }
+    }
+}
