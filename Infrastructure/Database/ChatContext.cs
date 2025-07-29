4769496fd226af46b9465a469b8b5236b4bf6e92
Octavio Ulla <oulla@ulasoft.com.ar>
2025-07-28 21:27:03 -0300

    fix(PI-26): No se habia creado ni configurado table LlamadaHerramientas

--- Infrastructure/Database/ChatContext.cs
+++ Infrastructure/Database/ChatContext.cs
@@ -6,6 +6,7 @@ using Infrastructure.Outbox;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,6 +23,12 @@ namespace Infrastructure.Database
         public DbSet<Chat> Chats { get; set; }
 
         public DbSet<MensajeIA> MensajesIA { get; set; }
+
+        public DbSet<MensajeTextoUsuario> MensajesTextoUsuario { get; set; }
+
+        public DbSet<MensajeLlamadaHerramienta> MensajesLlamadaHerramienta { get; set; }
+
+        public DbSet<MensajeHerramienta> MensajesHerramienta { get; set; }
 
         public DbSet<Document> Documents { get; set; }
 
@@ -42,6 +49,8 @@ namespace Infrastructure.Database
 
             ConfigureChatModel(modelBuilder.Entity<Chat>());
             ConfigureMensajeModel(modelBuilder.Entity<Mensaje>());
+            ConfigureMensajeLlamadaHerramientaModel(
+                modelBuilder.Entity<MensajeLlamadaHerramienta>());
             ConfigureDocumentoRecuperadoModel(
                 modelBuilder.Entity<DocumentoRecuperado>());
             ConfigureConsultaRecuperadaModel(
@@ -76,6 +85,19 @@ namespace Infrastructure.Database
             EntityTypeBuilder<Mensaje> mensajeBuilder)
         {
             mensajeBuilder.UseTpcMappingStrategy();
+        }
+
+        static void ConfigureMensajeLlamadaHerramientaModel(
+            EntityTypeBuilder<MensajeLlamadaHerramienta> llamadaHerramientaBuilder)
+        {
+            llamadaHerramientaBuilder
+                .Property(e => e.Argumentos)
+                .HasColumnType("json")
+                .HasConversion(
+                    v => JsonConvert.SerializeObject(v),
+                    v => JsonConvert.DeserializeObject<Dictionary<string, object>>(
+                        v)!)
+                .IsRequired();
         }
 
         static void ConfigureDocumentoRecuperadoModel(
